eda58e97b605e1fb6b7809a6e704a7f2eef10f43
Sergio Santoyo <ssantoyo@fb.com>
2020-07-14 08:20:00 -0700

    Initial Commit

---
+++ Yeet/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Yeet
+//
+//  Created by Sergio Santoyo on 7/14/20.
+//  Copyright © 2020 ssantoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Yeet/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Yeet
+//
+//  Created by Sergio Santoyo on 7/14/20.
+//  Copyright © 2020 ssantoyo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
